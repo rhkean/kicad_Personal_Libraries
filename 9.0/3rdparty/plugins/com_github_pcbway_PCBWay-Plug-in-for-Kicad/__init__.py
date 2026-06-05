259ef12524eeb7534587874a5fc1cf127d2b9775
Rob Kean <rhkean@gmail.com>
2026-06-05 14:21:15 -0400

    Untrack ignored folder

--- 9.0/3rdparty/plugins/com_github_pcbway_PCBWay-Plug-in-for-Kicad/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from .plugin import PCBWayPlugin
-    plugin = PCBWayPlugin()
-    plugin.register()
-except Exception as e:
-    import logging
-    root = logging.getLogger()
-    root.debug(repr(e))
--- 9.0/3rdparty/plugins/sierra_kicad_order/findpath.py
+++
@@ -1,1 +0,0 @@
-print(os.path.join(os.path.dirname(__file__), 'sierra_icon32.png'))
